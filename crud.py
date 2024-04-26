fbdf35b49c7ae79ffda5c687e8a29aaeb114e6ee
Ryan Wells <rswells2@gmail.com>
2024-04-26 13:02:22 -0600

    finished part 3

--- crud.py
+++ crud.py
@@ -10,11 +10,23 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+
+    return User.query.all()
+
 def create_movie(title, overview, release_date, poster_path):
 
     movie = Movie(title=title, overview= overview, release_date=release_date, poster_path=poster_path)
 
     return movie
+
+def get_movies():
+
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+
+    return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
 
--- server.py
+++ server.py
@@ -1,14 +1,47 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session,redirect)
+from model import connect_to_db, db
+import crud
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.app_context().push()
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route("/")
+def home():
+
+    return render_template("home.html")
+
+@app.route("/movies")
+def all_movies():
+
+    movies = crud.get_movies()
+
+    return render_template("all_movies.html", movies=movies)
+
+@app.route("/movies/<movie_id>")
+def show_movies(movie_id):
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template("movie_details.html", movie=movie)
+
+@app.route('/users')
+def all_users():
+
+    users = crud.get_users()
+
+    return render_template("all_users.html", users=users)
 
 
 # Replace this with routes and view functions!
 
 
 if __name__ == "__main__":
-
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
